36845981688042d5670d5dede4823999906c0fe2
Juanjo Garrido <garrido.juanjo@gmail.com>
2024-10-27 12:07:27 +0100

    updated script

--- scripts/run_fehm.sh
+++ scripts/run_fehm.sh
@@ -9,6 +9,7 @@ if [ -d "app/output/pared2d" ]; then
     mv app/output/pared2d/* ../FEHM/fehmpytests/escenarios/heat/pared/input/
     echo $PWD
     echo "Archivos movidos de app/output/pared2d a ../FEHM/fehmpytests/escenarios/heat/pared/input"
+    cd ../FEHM/fehmpytests/escenarios/heat/pared/input
 fi
 
 # Move files from app/output/pared3d to ../FEHM/fehmpytests/escenarios/heat/pared3d/input
@@ -16,6 +17,7 @@ if [ -d "app/output/pared3d" ]; then
     mv app/output/pared3d/* ../FEHM/fehmpytests/escenarios/heat/pared3d/input/
     echo "Archivos movidos de app/output/pared3d a ../FEHM/fehmpytests/escenarios/heat/pared3d/input"
     echo $PWD
+    cd ../FEHM/fehmpytests/escenarios/heat/pared3d/input
 fi
 
 # Move files from app/output/barovel to ../FEHM_mast/fehmpytests/escenarios/isotermal/baro_vel_V2/input
@@ -23,4 +25,8 @@ if [ -d "app/output/barovel" ]; then
     mv app/output/barovel/* ../FEHM_mast/fehmpytests/escenarios/isotermal/baro_vel_V2/input/
     echo "Archivos movidos de app/output/barovel a ../FEHM/fehmpytests/escenarios/isotermal/baro_vel_V2/input"
     echo $PWD
+    cd ../FEHM_mast/fehmpytests/escenarios/isotermal/baro_vel_V2/input
 fi
+
+# run FEHM
+xfehm control/fehmn.files
